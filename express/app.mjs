a1c025588360bbc28f631b7d0cce1aa410752703
yousefnegm <yousefnegm@gmail.com>
2024-02-07 08:02:55 +0200

    Add ejs dependency and update app.mjs

--- express/app.mjs
+++ express/app.mjs
@@ -4,14 +4,15 @@ import fetch from "node-fetch";
 const app = express();
 const port = 3030;
 
-let userArray = [];
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//using ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
-  res.body(JSON.stringify(userArray));
+  // res.send("Hello World");
+  res.render("index", { text: "World" });
 });
 
 app.put("/user", (req, res) => {
@@ -21,17 +22,16 @@ app.put("/user", (req, res) => {
 app.post("/user", (req, res) => {
   res.send("Got a post request at /user");
   // console.log(req.body);
-  userArray.push(req.body);
+  // userArray.push(req.body);
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-const response = await fetch("http://localhost:3030/user", {
-  method: "POST",
-  body: JSON.stringify({ name: "John Doe" }),
-  headers: { "Content-Type": "application/json" },
-});
-
-console.log(response);
+//i have to use http here for it to work with node-fetch
+// const response = await fetch("http://localhost:3030/user", {
+//   method: "POST",
+//   body: JSON.stringify({ name: "John Doe" }),
+//   headers: { "Content-Type": "application/json" },
+// });
