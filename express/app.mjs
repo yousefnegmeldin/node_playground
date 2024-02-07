7271bc7d325ac758310da16ab80311d918933a1a
yousefnegm <yousefnegm@gmail.com>
2024-02-07 07:46:06 +0200

    Add userArray to store user data and update routes

--- express/app.mjs
+++ express/app.mjs
@@ -1,13 +1,17 @@
 // const express = require("express");
 import express from "express";
+import fetch from "node-fetch";
 const app = express();
 const port = 3030;
+
+let userArray = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+  res.body(JSON.stringify(userArray));
 });
 
 app.put("/user", (req, res) => {
@@ -16,21 +20,18 @@ app.put("/user", (req, res) => {
 
 app.post("/user", (req, res) => {
   res.send("Got a post request at /user");
-  console.log(req.body);
+  // console.log(req.body);
+  userArray.push(req.body);
 });
-
-// function send(data) {
-//   fetch("localhost:3030/user", {
-//     method: "POST",
-//     body: JSON.stringify(data),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-// }
-
-// send({ name: "John Doe" });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+const response = await fetch("http://localhost:3030/user", {
+  method: "POST",
+  body: JSON.stringify({ name: "John Doe" }),
+  headers: { "Content-Type": "application/json" },
+});
+
+console.log(response);
