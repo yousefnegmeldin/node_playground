1e050ca383217270ddd7eb568180dfa5aeef2a75
yousefnegm <yousefnegm@gmail.com>
2024-02-07 08:21:37 +0200

    added router.param

--- express/routes/users.js
+++ express/routes/users.js
@@ -5,8 +5,20 @@ router.get("/", (req, res) => {
   res.send("user List");
 });
 
+//make sure to put all static above dynamic routes
 router.get("/new", (req, res) => {
   res.send("new user form");
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`user with id ${id}`);
+});
+
+//this can be helpful for async functions
+router.param("id", (req, res, next, id) => {
+  console.log("param middleware");
+  next();
 });
 
 module.exports = router;
