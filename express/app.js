8072d25890b09d77a1660d58d54aa6a70261755c
yousefnegm <yousefnegm@gmail.com>
2024-02-07 08:12:01 +0200

    added nested routing using express

--- express/app.js
+++ express/app.js
@@ -1,6 +1,5 @@
-// const express = require("express");
-import express from "express";
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
+const express = require("express");
 const app = express();
 const port = 3030;
 
@@ -11,23 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  // res.send("Hello World");
   res.render("index", { text: "World" });
 });
 
-app.put("/user", (req, res) => {
-  res.send("Got a PUT request at /user");
-});
+const userRouter = require("./routes/users.js");
 
-app.post("/user", (req, res) => {
-  res.send("Got a post request at /user");
-  // console.log(req.body);
-  // userArray.push(req.body);
-});
+//middleware for nested routers
+app.use("/users", userRouter);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(port);
 
 //i have to use http here for it to work with node-fetch
 // const response = await fetch("http://localhost:3030/user", {
